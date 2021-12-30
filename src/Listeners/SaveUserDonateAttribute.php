0b858a8e5805b393be72faecb8abb00f09b9f012
tagecode <tagecode@hotmail.com>
2021-12-30 19:17:07 +0800

    check donate is a url link

--- src/Listeners/SaveUserDonateAttribute.php
+++ src/Listeners/SaveUserDonateAttribute.php
@@ -2,6 +2,7 @@
 
 namespace TageCode\Donate\Listeners;
 
+use Flarum\Foundation\ValidationException;
 use Flarum\User\Event\Saving;
 use Flarum\User\Exception\PermissionDeniedException;
 use Illuminate\Support\Arr;
@@ -12,6 +13,7 @@ class SaveUserDonateAttribute
      * @param Saving $event
      *
      * * @throws PermissionDeniedException
+     * @throws ValidationException
      */
     public function handle(Saving $event)
     {
@@ -27,6 +29,14 @@ class SaveUserDonateAttribute
             if (!$isSelf) {
                 $actor->assertPermission($canEdit);
             }
+            // Check donate is a url link.
+            if (!empty($attributes['donate'])) {
+                if (!filter_var($attributes['donate'],
+                    FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED)) {
+                    throw new ValidationException(['message' => 'Donate must be a complete url link.']);
+                }
+            }
+
             $user->donate = $attributes['donate'];
             $user->save();
         }
